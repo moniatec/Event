6587009108625bd6504dfdf4cd3c73ed17b27c3b
monia <moniatec@gmail.com>
2021-03-25 09:49:37 -0400

    code clean up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-// import { NavLink } from 'react-router-dom';
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
