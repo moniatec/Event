3afb45bfd28652bb782c029f0a2c797bc874b3a9
monia <moniatec@gmail.com>
2021-04-12 13:55:49 -0400

    fix cors bug

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apiBaseUrl: process.env.NODE_ENV === "development" ?
         process.env.REACT_APP_API_BASE_URL :
-        "https://cors-anywhere.herokuapp.com/https://cool-events-back.herokuapp.com",
+        "https://cool-events-back.herokuapp.com",
     cloudinaryUrl: process.env.REACT_APP_CLOUDINARY_URL,
     cloudinaryPreset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
 }
